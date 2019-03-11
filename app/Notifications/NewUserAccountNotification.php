a5eb20f14b0819484547f73413feeb3a6421610e
kgoel085 <kgoel085@gmail.com>
2019-03-12 01:23:37 +0530

    Added mail notification for new user secret

---
+++ app/Notifications/NewUserAccountNotification.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+Class NewUserAccountNotification extends Notification
+{
+    use Queueable;
+    private $accountToken;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($accountNumber = null)
+    {
+        if($accountNumber) $this->accountToken = $accountNumber;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('Welcome. You are now verified member of '.env('APP_NAME').'. Your secret to access the API is : ')
+            ->line($this->accountToken)
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
+
+?>
