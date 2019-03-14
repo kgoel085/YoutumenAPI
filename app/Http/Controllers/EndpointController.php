62e4df2dce77326da20b5c189d12b89fda687695
kgoel085 <kgoel085@gmail.com>
2019-03-15 02:04:09 +0530

    Added requested api endpoint validations

--- app/Http/Controllers/EndpointController.php
+++ app/Http/Controllers/EndpointController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 
 class EndpointController extends Controller
 {
     private $requestObj;
     private $configObj;
     private $currentAction = null;
+    private $currentEndpoint = null;
 
     /**
      * Create a new controller instance.
@@ -46,10 +49,44 @@ class EndpointController extends Controller
         }
     }
 
+    public function checkAction(&$action){
+        $returnVal = false;
+
+        if($action || in_array($action, $this->configObj['AllowedEndpoints'])){
+            $this->currentAction = trim($action);
+            $this->currentEndpoint = $this->configObj['EndpointConfig'][$this->currentAction]['endpoint'];
+            $returnVal = true;
+        }
+
+        if(!$this->currentEndpoint) $returnVal = false;
+
+        return $returnVal;
+    }
+
+    public function checkParameters(){
+        $returnArr = array('Status' => true, 'msg' => array());
+
+        $receivedParams = $this->requestObj->all();
+        $allowedParams = array_keys($this->configObj['EndpointConfig'][$this->currentAction]['params']);
+
+        if(count($receivedParams) > 0){
+            foreach($receivedParams as $reqVars){
+                if(!in_array($reqVars, $allowedParams)) $returnArr['msg'][] = $reqVars;
+            }
+        }
+
+        if(count($returnArr['msg']) > 0){
+            $returnArr['Status'] = false;
+            $returnArr['msg'] = implode(', ', array_unique($returnArr['msg']));
+        }
+
+        return $returnArr;
+    }
+
     /**
-     * All the rquests will be validated and then passed on 
+     * All the actions will be validated and then will be executed
      */
-    public function getAction($action = null){
+    public function performAction($action = null){
         //Check if current action is allowed or not
         if(!$this->checkAction($action)){
             return response()->json([
@@ -59,26 +96,40 @@ class EndpointController extends Controller
 
         //Check if request parameters are also allowed or not
         $validParams = $this->checkParameters();
-        if(!$validParams['Status']){
+        if(!$validParams['Status'] || $validParams['msg']){
             return response()->json([
-                'error' => 'Following parameters are not allowed. Invalid parameters: '
+                'error' => 'Following parameters are not allowed. Invalid parameters: '.$validParams['msg']
             ], 400);
         }
 
+        try{
+            //Prepare cURL client with configuration
+            $client = new Client([
+                'base_uri' => $this->configObj['Global']['url']
+            ]);
 
-        
-    }
+            // Send a request
+            $response = $client->request('GET', $this->currentEndpoint, [
+                'query' => ['key' => env('YOUTUBE_KEY'), 'part' => 'snippet,contentDetails'],
+                'headers' => ['Referer' => env('APP_URL'), 'Accept'     => 'application/json']
+            ]);
+        }catch(ClientException $e){
+            $error = [
+                'status' => 400,
+                'message' => 'Error Ocurred !'
+            ];
+            $response = $e->getResponse();
+            $responseBodyAsString = $response->getBody()->getContents();
 
-    public function checkAction(&$action){
-        $returnVal = false;
-
-        if($action || in_array($action, $this->configObj['AllowedEndpoints'])){
-            $this->currentAction = trim($action);
-            $returnVal = true;
+            if(empty($responseBodyAsString) == false){
+                $tmpStr = json_decode($responseBodyAsString, true);
+                if($tmpStr['error']['code']) $error['status'] = $tmpStr['error']['code'];
+                if($tmpStr['error']['message']) $error['message'] = $tmpStr['error']['message'];
+            }
+            
+            return response()->json([
+                'error' => $error['message']
+            ], $error['status']);
         }
-
-        return $returnVal;
     }
-
-
 }
