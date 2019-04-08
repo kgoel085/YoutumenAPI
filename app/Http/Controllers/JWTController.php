9ab85475f1bcc957b78c50ec201b3eb319ddf14e
kgoel085 <kgoel085@gmail.com>
2019-04-09 01:22:44 +0530

    Added options to provide extra values for JWT

--- app/Http/Controllers/JWTController.php
+++ app/Http/Controllers/JWTController.php
@@ -16,6 +16,7 @@ class JWTController extends Controller
 {
 
     private $reqVars;
+    private $authUser;
 
     /**
      * Create a new controller instance.
@@ -25,6 +26,9 @@ class JWTController extends Controller
     public function __construct(Request $request)
     {
         if($request) $this->reqVars = $request;
+        
+        //Validate current user
+        $this->validateUser();
     }
 
     /**
@@ -42,46 +46,63 @@ class JWTController extends Controller
         ]);
     }
 
+    public function validateUser(){
+       $authUSer = $this->reqVars->auth;
+       if($authUSer) $this->authUser = $authUSer;
+
+        if(!$this->authUser){
+
+            //Validate the params
+            $this->validator($this->reqVars);
+
+            //Validate the user
+            $user = User::where([['email', '=', $this->reqVars->input('email')], ['account', '=', $this->reqVars->input('account')]])->first();
+            if(!$user){
+                return response()->json(['error' => 'Invalid login provided.'], 401);
+            }
+
+            //validate the password
+            if (!Hash::check($this->reqVars->input('password'), $user->password)) {
+                return response()->json(['error' => 'Invalid password provided'], 401);
+            }
+
+            $this->authUser = $user;
+        }
+    }
+
     /**
      * Creates the JWT token based on received user
      * 
      * @param App\User
      * @return string \Firebase\JWT\JWT
      */
-    protected function jwtToken(User $user){
+    public function jwtToken($arr = array()){
         $payload = [
             'iss' => Hash::make(env('APP_NAME')),
-            'sub' => $user->id,
+            'sub' => $this->authUser->id,
             'iat' => time(),
             'exp' => time() + env('JWT_EXPIRY', 10)*60
         ];
+
+        if(count($arr) > 0) $payload = array_merge($arr, $payload);
+
         return JWT::encode($payload, env('JWT_SECRET'));
     }
 
     /**
      * Validate received credentials and return JWT token
      */
-    public function generateToken(){
-        //Validate the params
-        $this->validator($this->reqVars);
-        
-        //Validate the user
-        $user = User::where([['email', '=', $this->reqVars->input('email')], ['account', '=', $this->reqVars->input('account')]])->first();
-        if(!$user){
-            return response()->json(['error' => 'Invalid login provided.'], 401);
-        }
-        //validate the password
-        if (!Hash::check($this->reqVars->input('password'), $user->password)) {
-            return response()->json(['error' => 'Invalid password provided'], 401);
-        }
-        
-        $response = response()->json(['success' => ['token' => $this->jwtToken($user)]], 200);
+    public function generateToken($tokenExtras = array()){
+        $newToken = $this->jwtToken($tokenExtras);
+        $response = response()->json(['success' => ['token' => $newToken]], 200);
 
         if(env('JWT_COOKIE') == true && env('JWT_COOKIE_NAME', null)){
-            $cookieName = Hash::make(env('JWT_COOKIE_NAME'));
-            $response = $response->withCookie(new Cookie($cookieName, $this->jwtToken($user), env('JWT_EXPIRY')));
+            $cookieName = env('JWT_COOKIE_NAME');
+            $response = $response->withCookie(new Cookie($cookieName, $newToken, env('JWT_EXPIRY')));
         }
 
         return $response;
     }
+
+    
 }
