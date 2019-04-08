cc97a2a33b4c9a4c7664db6be1c5f3bfa7bf74da
kgoel085 <kgoel085@gmail.com>
2019-04-09 01:51:33 +0530

    Added controller to control authorized requests

---
+++ app/Http/Controllers/GoogleAuthController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Traits\GoogleAuthTrait;
+
+class GoogleAuthController extends Controller
+{
+    use GoogleAuthTrait;
+}
