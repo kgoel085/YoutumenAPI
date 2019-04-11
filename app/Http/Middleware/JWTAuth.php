dc99090bf145affe83cb9b93548ea7318e16112d
kgoel085 <kgoel085@gmail.com>
2019-04-12 00:03:12 +0530

    Added Google auth token check

--- app/Http/Middleware/JWTAuth.php
+++ app/Http/Middleware/JWTAuth.php
@@ -9,6 +9,7 @@ use App\User;
 Use Firebase\JWT\JWT;
 use Firebase\JWT\ExpiredException;
 use Illuminate\Support\Facades\Hash;
+use App\UserGoogleToken;
 
 class JWTAuth
 {
@@ -73,6 +74,36 @@ class JWTAuth
 
             //Add decoded token details in current request
             $request->jwt = $credentials;
+        }
+
+
+        //Check if google auth is enabled on the server or not
+        if(!env('GOOGLE_AUTH_ENABLED') || !class_exists('Google_Client') || !array_key_exists('jwt', get_object_vars($request))){
+            return response()->json([
+                'error' => 'Service currently unavailable. Please try again later'
+            ], 503);
+        }
+
+        //Check if google user authorized token exists or not
+        if($request->jwt->authToken){
+            $userTokenObj = UserGoogleToken::where([['token', '=', $request->jwt->authToken], ['user_id', '=', $request->jwt->sub]])->first();
+            if(!$userTokenObj){
+                return response()->json([
+                    'error' => 'Invalid / Unauthorization token provided'
+                ], 400);
+            }
+
+            if(!array_key_exists('google_token', $request->jwt)){
+                $gAuthArr = json_decode($userTokenObj->g_auth_token, true);
+                if(!array_key_exists('access_token', $gAuthArr)){
+                    return response()->json([
+                        'error' => 'Invalid / Unauthorization token provided'
+                    ], 400);
+                }
+        
+                $accessToken = $gAuthArr['access_token'];
+                if($accessToken) $request->jwt->google_token = $accessToken;
+            }
         }
 
         //Check if configuration file exists or not 
